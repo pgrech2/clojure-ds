e556f4899678c0e1cbc87c75c1060f141b9ba590
Paul Grech <pgrech@theladders.com>
2018-04-16 16:20:16 -0400

    Cleanup project file

--- project.clj
+++ project.clj
@@ -9,30 +9,37 @@
                  [com.taoensso/timbre "4.10.0"]
                  [com.stuartsierra/component "0.3.2"]
 
+                 ;; Utilities
+                 [cheshire "5.7.0"]
                  [clj-time "0.14.2"]
                  [clj-http "3.7.0"]
 
-                 ;; R Like statistical library
-                 [incanter "1.5.7"]
+                 ;; Random Sampling
+                 [bigml/sampling "3.2"]
 
-                 ;; Data visualization
-                 [metasoarous/oz "1.2.1"]
-
-                 ;; Matrix libraries
+                 ;; Matrix
                  [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]
                  [clatrix "0.5.0"]
                  ;; Requires further installation
                  ;; [uncomplicate/neanderthal "0.18.0"]
 
-                 ;; Neural Network toolkit
+                 ;; Visualization
+                 [metasoarous/oz "1.3.1"]
+
+                 ;; MACHINE LEARNING Libraries
+                 [incanter/incanter "1.5.7"]
+                 [lambda-ml "0.1.1"]
+                 ;; Forward chaining rule
+                 [com.cerner/clara-rules "0.17.0"]
+
+                 ;; Neural networks
                  [thinktopic/cortex "0.9.22"]
-
-                 ;; NLP
-                 [org.clojurenlp/core "3.7.0"]
-
-                 ;; Machine Learning
-                 [lambda-ml "0.1.1"]]
+                 ;; Not yet released
+                 ;; Tensorflow
+                 ;; https://github.com/bpiel/guildsman
+                 ;; Statistical Library
+                 ]
   :resource-paths ["resources"]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component.repl "0.2.0"]]
